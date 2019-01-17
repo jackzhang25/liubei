5ffc18916f4ecdbcf551a6256ab41fdfec9dcd53
jackzhang25 <jackzhang25@live.com>
2019-01-16 21:30:22 -0800

    Update FrogJump.py

--- FrogJump.py
+++ FrogJump.py
@@ -2,8 +2,8 @@ def solution(X, Y, D):
     if X >= Y:
         return 0 
     # write your code in Python 3.6
-    distance = Y - X
-    distance /= D
-    if distance == int(distance):
-        return int(distance)
-    return int(distance) + 1  
+    distance = (Y - X)/D
+    int_dis = int(distance)
+    if distance == int_dis:
+        return int_dis
+    return int_dis + 1  
